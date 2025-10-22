a16c45ad4628ac13626bf258b63b5e590a463bac
Akifcan <akfkara97@gmail.com>
2025-10-22 15:32:03 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Monoracle - Get your own data from chain
+ * @packageDocumentation
+ */
+
+export const version = '1.0.0';
+
+/**
+ * Example function
+ * @param message - The message to display
+ * @returns The formatted message
+ */
+export function hello(message: string): string {
+  return `Monoracle says: ${message}`;
+}
+
+console.log(hello('adfas'))
